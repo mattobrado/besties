fd44c076d599e6d189c8f63264bb71cbd188a9a6
mattobrado <mcobradovic@gmail.com>
2023-10-06 12:28:10 -0400

    add redirecting and remove toast

--- src/hooks/reviews.ts
+++ src/hooks/reviews.ts
@@ -1,13 +1,14 @@
-import { useToast } from "@chakra-ui/react";
 import { setDoc, doc } from "firebase/firestore";
 import { useState } from "react";
 import { db } from "../lib/firebase";
 import { uuidv4 } from "@firebase/util";
 import { ReviewType } from "../lib/types";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../lib/routes";
 
 export const useAddReview = () => {
   const [isLoading, setLoading] = useState(false);
-  const toast = useToast();
+  const navigate = useNavigate();
 
   const addReview = async (review: ReviewType) => {
     setLoading(true);
@@ -18,14 +19,8 @@ export const useAddReview = () => {
       date: Date.now(),
       likes: [],
     });
-    toast({
-      title: "Post added successfully!",
-      status: "success",
-      isClosable: true,
-      position: "top",
-      duration: 5000,
-    });
     setLoading(false);
+    navigate(ROUTES.HOME);
   };
 
   return { addReview, isLoading };
