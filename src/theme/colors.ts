3f3fedb9995ae22926c2dca4c2fb24a3d14a7502
mattobrado <mcobradovic@gmail.com>
2023-10-19 12:45:26 -0400

    styling

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -3,10 +3,11 @@ export const COLORS = {
   BACKGROUND: "transparent",
   BRAND: "white",
   COLOR_SCHEME: "whiteAlpha",
-  PLACEHOLDER: "gray.100",
+  PLACEHOLDER: "white",
 };
 
 export const BACKGROUNDS = {
   RAINBOW_MESH:
     "radial-gradient(at 98% 69%, rgb(155, 93, 229) 0px, transparent 50%), radial-gradient(at 89% 12%, rgb(241, 91, 181) 0px, transparent 50%), radial-gradient(at 27% 51%, rgb(255, 153, 200) 0px, transparent 50%), radial-gradient(at 82% 85%, rgb(112, 214, 255) 0px, transparent 50%), radial-gradient(at 3% 34%, rgb(255, 112, 166) 0px, transparent 50%), radial-gradient(at 0% 18%, rgb(255, 151, 112) 0px, transparent 50%), radial-gradient(at 2% 70%, rgb(255, 214, 112) 0px, rgb(237, 247, 187) 50%)",
+  BLUE: "linear-gradient(90deg, rgb(128, 233, 255), rgb(128, 169, 255))",
 };
