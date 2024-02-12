19126503a2ac5dc2a579b2fde5053e2abdff2bec
mattobrado <mcobradovic@gmail.com>
2024-02-12 17:52:34 -0500

    fix dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { ColorModeScript } from "@chakra-ui/react";
 import App from "src/App";
-import { customTheme } from "src/lib";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ColorModeScript initialColorMode={customTheme.config.initialColorMode} />{" "}
+    {/* <ColorModeScript initialColorMode={customTheme.config.initialColorMode} />{" "} */}
     <App />
   </React.StrictMode>
 );
