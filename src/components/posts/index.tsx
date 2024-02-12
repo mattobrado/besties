6b0c924d66e075c7a2951244091d4958b5df0e3a
mattobrado <mcobradovic@gmail.com>
2024-02-11 21:35:24 -0500

    removes relative imports

--- src/components/posts/index.tsx
+++ src/components/posts/index.tsx
@@ -1,3 +1,5 @@
 export { default as PostList } from "./PostList";
 export { default as PostFeed } from "./PostFeed";
 export { default as OneUserPost } from "./OneUserPost";
+export { default as PostBody } from "./PostBody";
+export { default as TimeStamp } from "./TimeStamp";
